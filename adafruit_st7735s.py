ad3dc7b82002b6a8537217a24daee7e190d3c071
toxicantidote <23189273+toxicantidote@users.noreply.github.com>
2023-07-12 17:35:13 +0930

    Update and rename adafruit_st7735r.py to adafruit_st7735s.py
    Initial test. Still need to hookup hardware

--- adafruit_st7735s.py
+++ adafruit_st7735s.py
@@ -4,28 +4,23 @@
 # SPDX-License-Identifier: MIT
 
 """
-`adafruit_st7735r`
+`adafruit_st7735s`
 ====================================================
 
-Displayio driver for ST7735R based displays.
+Displayio driver for ST7735S based displays.
 
 * Author(s): Scott Shawcroft and Melissa LeBlanc-Williams
+Modified by Toxicantidote to suit ST7735S based displays
 
 Implementation Notes
 --------------------
 
 **Hardware:**
 
-* `1.8" SPI TFT display, 160x128 18-bit color
-  <https://www.adafruit.com/product/618>`_ (Product ID: 618)
-* `Adafruit 0.96" 160x80 Color TFT Display w/ MicroSD Card Breakout
-  <https://www.adafruit.com/product/3533>`_ (Product ID: 3533)
-* `1.8" Color TFT LCD display with MicroSD Card Breakout:
-  <https://www.adafruit.com/product/358>`_ (Product ID: 358)
-* `Adafruit 1.44" Color TFT LCD Display with MicroSD Card breakout:
-  <https://www.adafruit.com/product/2088>`_ (Product ID: 2088)
-* `Adafruit Mini Color TFT with Joystick FeatherWing:
-  <https://www.adafruit.com/product/3321>`_ (Product ID: 3321)
+ST7735S based displays.
+
+Tested with the DFRobot 1.8in TFT display (SKU: DFR0928)
+<https://wiki.dfrobot.com/SKU_DFR0928_Fermion_1.8_Inches_128_160_IPS_TFT_LCD_Display_with_MicroSD_Card_Slot_Breakout>
 
 **Software and Dependencies:**
 
@@ -45,34 +40,33 @@ __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_ST7735R.git"
 
 _INIT_SEQUENCE = bytearray(
-    b"\x01\x80\x96"  # SWRESET and Delay 150ms
-    b"\x11\x80\xff"  # SLPOUT and Delay
-    b"\xb1\x03\x01\x2C\x2D"  # _FRMCTR1
-    b"\xb2\x03\x01\x2C\x2D"  # _FRMCTR2
-    b"\xb3\x06\x01\x2C\x2D\x01\x2C\x2D"  # _FRMCTR3
-    b"\xb4\x01\x07"  # _INVCTR line inversion
+    b"\x01\x11\x80\x78"  # Exit sleep and Delay 150ms  
+    b"\xb1\x03\x01\x2C\x2D"  # Frame rate control (normal mode)
+    b"\xb2\x03\x01\x2C\x2D"  # Frame rate control (idle mode)
+    b"\xb3\x06\x01\x2C\x2D\x01\x2C\x2D"  # Frame rate control (local mode)
+    b"\xb4\x01\x07"  # Reverse control
+    b"\xb6\x03\xA2\x02\x84"  # Frame rate control (idle mode)  
     b"\xc0\x03\xa2\x02\x84"  # _PWCTR1 GVDD = 4.7V, 1.0uA
     b"\xc1\x01\xc5"  # _PWCTR2 VGH=14.7V, VGL=-7.35V
     b"\xc2\x02\x0a\x00"  # _PWCTR3 Opamp current small, Boost frequency
-    b"\xc3\x02\x8a\x2a"
-    b"\xc4\x02\x8a\xee"
+    b"\xc3\x02\x8a\x2a" # More power consumption control
+    b"\xc4\x02\x8a\xee" # More power consumption control
     b"\xc5\x01\x0e"  # _VMCTR1 VCOMH = 4V, VOML = -1.1V
     b"\x20\x00"  # _INVOFF
-    b"\x36\x01\x18"  # _MADCTL bottom to top refresh
-    # 1 clk cycle nonoverlap, 2 cycle gate rise, 3 sycle osc equalie,
-    # fix on VTL
-    b"\x3a\x01\x05"  # COLMOD - 16bit color
-    b"\xe0\x10\x02\x1c\x07\x12\x37\x32\x29\x2d\x29\x25\x2B\x39\x00\x01\x03\x10"  # _GMCTRP1 Gamma
-    b"\xe1\x10\x03\x1d\x07\x06\x2E\x2C\x29\x2D\x2E\x2E\x37\x3F\x00\x00\x02\x10"  # _GMCTRN1
-    b"\x13\x80\x0a"  # _NORON
-    b"\x29\x80\x64"  # _DISPON
+    b"\x3a\x01\x05" # Colour mode setting (16 bit)
+    b"\x36\x01\xc0"  # _MADCTL bottom to top refresh
+    b"\x2a\x04\x00\x00\x00\x4f" # Memory data access control commands
+    b"\x2b\x04\x00\x00\x00\x9f" # Memory data access control commands
+    b"\xe0\x10\x04\x22\x07\x0a\x2e\x30\x25\x2a\x28\x26\x2e\x3a\x00\x01\x03\x13"  # _GMCTRP1 Gamma
+    b"\xe1\x10\x04\x16\x06\x0d\x2d\x26\x23\x27\x27\x25\x2d\x3b\x00\x01\x04\x13"  # _GMCTRN1
+    b"\x01\x29\x00\x00"
 )
 
 
 # pylint: disable=too-few-public-methods
-class ST7735R(displayio.Display):
+class ST7735S(displayio.Display):
     """
-    ST7735R display driver
+    ST7735S display driver
 
     :param displayio.FourWire bus: bus that the display is connected to
     :param bool bgr: (Optional) An extra init sequence to append (default=False)
